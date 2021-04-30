e4391a247e47172dd7c0824a60e0e475929af2ff
Rehan2000 <62906395+Rehan2000@users.noreply.github.com>
2021-04-30 15:53:18 +0200

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,73 @@
+const GAME_CLOCK = 100
+const BLOCK_SIDE_LENGTH = 30
+const ROWS = 20
+const COLS = 10
+
+const SCORE_WORTH = 100
+const SCORE_onepiece = 10
+const holesbeneath = -30
+const noholesbeneath = 10
+const scorenewline = 1000
+
+
+const endposition = -100
+const SHAPES = [
+    [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+    ], [
+        [1, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ], [
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
+    ], [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ], [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0]
+    ], [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0]
+    ], [
+        [1, 1],
+        [1, 1]
+    ],
+]
+/*const COLORS = [
+    '#000000',
+    '#72CB3B',
+    '#FFD500',
+    '#FF971C',
+    '#FF3213',
+]*/
+const COLORS = [
+    '#000000',
+    '#F76F00',
+    '#F7EB00',
+    '#0DF700',
+    '#00F7D7',
+    '#009BF7',
+    '#026EFF',
+    '#FF0202',
+]
+
+
+
+const COLORS1 = [
+    '#F76F00',
+    '#F7EB00',
+    '#0DF700',
+    '#00F7D7',
+    '#009BF7',
+    '#026EFF',
+    '#FF0202',
+]
---
+++ script.js
@@ -0,0 +1,72 @@
+
+let canvas = document.getElementById("game-canvas") 
+let scoreboard = document.getElementById("scoreboard") 
+let ctx = canvas.getContext("2d") 
+ctx.scale(BLOCK_SIDE_LENGTH, BLOCK_SIDE_LENGTH) 
+let model = new gamemodel(ctx)
+
+let scoree = 0 
+p=1;
+
+
+let newGameState = () => {
+    fullSend() 
+    if (model.fallingPiece === null&&p==1) {
+        const rand = Math.floor(Math.random() * 7)
+        const newPiece = new piece(SHAPES[rand], ctx) 
+        model.fallingPiece = newPiece 
+        newfallingpiece();
+        model.moveDown()
+        scoree += SCORE_onepiece
+        scoreboard.innerHTML = "Score: " + String(scoree)
+       p=0
+    } else {
+        model.moveDown() 
+    }
+}
+
+const fullSend = () => {
+    const allFilled = (row) => {
+        for (let x of row) {
+            if (x === 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+    for (let i = 0; i < model.grid.length; i++) {
+        if (allFilled(model.grid[i])) {
+            scoree += SCORE_WORTH 
+            model.grid.splice(i, 1) 
+            model.grid.unshift([0,0,0,0,0,0,0,0,0,0])
+        }
+    }
+  
+    scoreboard.innerHTML = "Score: " + String(scoree)
+
+}
+
+document.addEventListener("keydown", (e) => {
+    e.preventDefault() 
+    switch(e.key) {
+        case "z":
+            model.rotate() 
+            break 
+        case "d":
+            model.move(true) 
+            break 
+        case "s": 
+            model.moveDown() 
+            break 
+        case "q":
+            model.move(false) 
+            break
+    }
+})
+function start(){
+   setInterval(() => {
+      newGameState()
+  }, GAME_CLOCK); 
+  
+}
